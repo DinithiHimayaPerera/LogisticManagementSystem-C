ce15274d90e53203bf6ca2d1149443a1a425044c
Dinithi Himaya <as20240455@sci.sjp.ac.lk>
2025-10-22 23:21:03 +0530

    add do while loop to print the main menu

--- main.c
+++ main.c
@@ -30,13 +30,94 @@ void printEstimation(float deliveryCost, float fuelUsed, float fuelCost,float to
 
 int main()
 {
+    int choice;
+    char cityName[MAX_CITIES][NAME_LENGTH];
+    int cityCount=0;
+    int distance[MAX_CITIES][MAX_CITIES];
 
+    printf("\tLogistic Management System");
+    printf("\n\t==========================");
 
+do{
+        printf("\n\n\tMain Menu\n\n");
         printf("1. City Management\n");
         printf("2. Distance Management\n");
         printf("3. Delivery Request Handling and Delivery Cost\n");
         printf("4. Exit \n");
 
+        printf("\nEnter the choice :");
+        scanf("%d",&choice);
+
+        switch(choice){
+
+    case 1:
+        cityManagement(cityName,&cityCount);
+        break;
+
+    case 2:
+        distanceManagement(cityName,cityCount,distance);
+        break;
+
+    case 3:
+        deliveryRequest(cityName,cityCount,distance);
+        break;
+
+    case 4:
+        printf("\nExit from the Logistic Management System................!\n");
+        break;
+
+    default:
+        printf("\nInvalid Choice.\n");
+
+
+        }
+
+}while(choice!=4);
+
 
     return 0;
+}
+void cityManagement(char cityName[MAX_CITIES][NAME_LENGTH],int *cityCount)
+{
+
+}
+void addCity(char cityName[MAX_CITIES][NAME_LENGTH],int *cityCount)
+{
+
+}
+void removeCity(char cityName[MAX_CITIES][NAME_LENGTH],int *cityCount)
+{
+
+}
+void renameCity(char cityName[MAX_CITIES][NAME_LENGTH],int *cityCount)
+{
+
+}
+void checkRepetition(char cityName[MAX_CITIES][NAME_LENGTH],int cityCount, int newCityId)
+{
+
+}
+void distanceManagement(char cityName[MAX_CITIES][NAME_LENGTH],int cityCount, int distance[MAX_CITIES][MAX_CITIES])
+{
+
+}
+void printChart(char cityName[MAX_CITIES][NAME_LENGTH],int cityCount, int distance[MAX_CITIES][MAX_CITIES])
+{
+
+}
+void deliveryRequest(char cityName[MAX_CITIES][NAME_LENGTH],int cityCount, int distance[MAX_CITIES][MAX_CITIES])
+{
+
+}
+int minimumDistance(char cityName[MAX_CITIES][NAME_LENGTH],int cityCount, int distance[MAX_CITIES][MAX_CITIES], int start, int end)
+{
+
+}
+void calculateCost(int distance, float weight, int vehicleIndex, float *deliveryCostLast,float fuelUsedLast,float *fuelCostLast, float *totalCostLast,float *profitLast, float *finalChargeLast,float *estimateTimeLast)
+{
+
+}
+void printEstimation(float deliveryCost, float fuelUsed, float fuelCost,float totalCost, float profit, float finalCharge,float estimateTime,const char* startCity, const char* endCity,const char* vehicleType, float weight, int distance)
+{
+
 }
