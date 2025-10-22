468404174820bae2bbc763766621676bf4309e7e
Dinithi Himaya <as20240455@sci.sjp.ac.lk>
2025-10-22 21:36:44 +0530

    print global variables

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+#define MAX_CITIES 30
+#define NAME_LENGTH 50
+#define  FUEL_PRICE 310.00
+#define VEHICLES 3
+
+int main()
+{
+
+    return 0;
+}
